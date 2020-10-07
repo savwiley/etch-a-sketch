1f8101704801a052ef071601720134603331d143
Savannaha <savwiley@yahoo.com>
2020-10-07 14:23:17 -0500

    modified js

---
+++ eas.js
@@ -0,0 +1,12 @@
+//grid
+const grid = document.querySelector("#grid");
+
+//creates squares
+function addSquares(numb = 16) {
+    let sum = numb * numb;
+    for (let i = 0; i < sum; i++) {
+        const squares = document.createElement("div");
+        squares.classList.add("square");
+        grid.appendChild(squares);
+    } return;
+}
